d24229b254e21b4fbe7f1b52baace797dcdaf511
Sander Schep <s.c.schep@gmail.com>
2018-06-07 15:15:02 +0200

    POC Added HLS support default controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ import {VgDASH} from "videogular2/src/streaming/vg-dash/vg-dash";
 export class AppComponent {
   // @ViewChild(VgDASH) vgDash: VgDASH;
   stream = {
-    type: 'dash',
-    source: 'https://bitmovin-a.akamaihd.net/content/playhouse-vr/mpds/105560.mpd',
+    // source: 'https://bitmovin-a.akamaihd.net/content/playhouse-vr/mpds/105560.mpd',
+    source: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
   };
   // bitrates: BitrateOption[];
   //
