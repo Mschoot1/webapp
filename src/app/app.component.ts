d79d81803ef109e5779398d09672cedbaa3edfaf
Sander Schep <s.c.schep@gmail.com>
2018-06-07 14:17:35 +0200

    Dash POC default controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {BitrateOption} from "videogular2/core";
+import {VgDASH} from "videogular2/src/streaming/vg-dash/vg-dash";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // @ViewChild(VgDASH) vgDash: VgDASH;
+  stream = {
+    type: 'dash',
+    source: 'https://bitmovin-a.akamaihd.net/content/playhouse-vr/mpds/105560.mpd',
+  };
+  // bitrates: BitrateOption[];
+  //
+  // setBitrate(option: BitrateOption) {
+  //   this.vgDash.setBitrate(option);
+  //   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {VgCoreModule} from 'videogular2/core';
+import {VgControlsModule} from 'videogular2/controls';
+import {VgOverlayPlayModule} from 'videogular2/overlay-play';
+import {VgBufferingModule} from 'videogular2/buffering';
+import { VgStreamingModule } from "videogular2/streaming";
 
 import { AppComponent } from './app.component';
 
@@ -10,6 +14,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
+    VgStreamingModule,
     BrowserModule
   ],
   providers: [],
