22e03d6dffc6a4347bbe4a6aa8fa5d4519179702
Mschoot1 <marnix_vd_schoot@hotmail.com>
2018-06-08 11:10:05 +0200

    clientSocket working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ChatService} from "./app.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ChatService]
 })
+
 export class AppComponent {
   title = 'app';
+  messages = [];
+
+  constructor(private chatService: ChatService) {
+  }
+
+  ngOnInit() {
+    this.chatService
+      .getMessage()
+      .subscribe(message => {
+        this.messages.push(message);
+      });
+  }
+
+  sendMessage(msg) {
+    this.chatService.sendMessage(msg);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
 
 import { AppComponent } from './app.component';
 
+const config: SocketIoConfig = { url: 'https://calm-ravine-53383.herokuapp.com/', options: {} };
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
