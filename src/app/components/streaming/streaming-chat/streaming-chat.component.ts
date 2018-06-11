2d37f0de11df2c35fd64279908e50a051b24dcf5
sander9512 <s.c.schep@gmail.com>
2018-06-11 23:04:21 +0200

    Added basic chatbox -- needs positioning fix

--- src/app/components/streaming/streaming-chat/streaming-chat.component.ts
+++ src/app/components/streaming/streaming-chat/streaming-chat.component.ts
@@ -6,9 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./streaming-chat.component.css']
 })
 export class StreamingChatComponent implements OnInit {
+  messages = [];
+  // messages =[{
+  //   "text": "Hi how are you?",
+  //   "self":false
+  // }, {
+  //   "text": "I am fine",
+  //   "self": true
+  // }];
+  replyMessage = "";
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  reply() {
+    this.messages.push({
+      "text": this.replyMessage,
+      "self":true
+    })
+    this.replyMessage = "";
   }
 }
