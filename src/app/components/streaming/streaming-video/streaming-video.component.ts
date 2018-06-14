adfb93513e4b6e49424bf75b5286fa701707a554
Sander Schep <s.c.schep@gmail.com>
2018-06-14 13:16:06 +0200

    Chatbox changed, video player size fixed

--- src/app/components/streaming/streaming-video/streaming-video.component.ts
+++ src/app/components/streaming/streaming-video/streaming-video.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {BitrateOption, VgAPI} from 'videogular2/core';
 import {VgDASH} from 'videogular2/src/streaming/vg-dash/vg-dash';
 import { VgHLS } from 'videogular2/src/streaming/vg-hls/vg-hls';
+import {Subscription} from 'rxjs/Rx';
+import {TimerObservable} from 'rxjs-compat/observable/TimerObservable';
 
 
 export interface IMediaStream {
@@ -18,7 +20,6 @@ export class StreamingVideoComponent implements OnInit {
   @ViewChild(VgDASH) vgDash: VgDASH;
   @ViewChild(VgHLS) vgHls: VgHLS;
   currentStream: IMediaStream;
-  api: VgAPI;
 
   bitrates: BitrateOption[];
   streams: IMediaStream[] = [
@@ -35,15 +36,25 @@ export class StreamingVideoComponent implements OnInit {
       source: 'http://www.streambox.fr/playlists/test_001/stream.m3u8'
     }
   ];
-  constructor() {
-  }
-
-  ngOnInit() {
-    this.currentStream = this.streams[ 1 ];
+  constructor(private api: VgAPI) {
   }
   onPlayerReady(api: VgAPI) {
     this.api = api;
     console.log('api', api);
+  }
+  ngOnInit() {
+   this.currentStream = this.streams[ 2 ];
+  }
+  onClickStream(stream: IMediaStream) {
+    this.api.pause();
+    this.bitrates = null;
+
+    const timer: Subscription = TimerObservable.create(0, 10).subscribe(
+      () => {
+        this.currentStream = stream;
+        timer.unsubscribe();
+      }
+    );
   }
 
   setBitrate(option: BitrateOption) {
