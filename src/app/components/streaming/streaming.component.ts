14808db1a2e7a9adcd0965c8a5adf8622766ea1f
Sander Schep <s.c.schep@gmail.com>
2018-06-15 10:31:48 +0200

    Message model created + timestamp for messages

--- src/app/components/streaming/streaming.component.ts
+++ src/app/components/streaming/streaming.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import {TempStreamerService} from '../../services/temp-streamer.service';
 
 @Component({
   selector: 'app-streaming',
   templateUrl: './streaming.component.html',
-  styleUrls: ['./streaming.component.css']
+  styleUrls: ['./streaming.component.css'],
+  providers: [TempStreamerService]
 })
 export class StreamingComponent implements OnInit {
-  name = '';
+  streamer;
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router, private tempStreamers: TempStreamerService) { }
 
   ngOnInit() {
     this.route.params
       .subscribe(params => {
         console.log('params', params);
         console.log('name ' + params['name']);
-        this.name = params['name'];
+        this.streamer = this.tempStreamers.getStreamer(params['name']);
+        console.log('streamer: ', this.streamer);
       });
   }
 
